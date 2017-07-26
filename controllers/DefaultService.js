86ea62d4f32b131035237276ff09495028a22dd4
Julian Frank <julianf@hcl.com>
2017-07-26 15:25:53 +0530

    basic swagger based app ready...code to be plugged in next

---
+++ controllers/DefaultService.js
@@ -0,0 +1,26 @@
+'use strict';
+
+exports.action_handler = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  **/
+  // no response value expected for this operation
+  res.end();
+}
+
+exports.call_handler = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  **/
+  // no response value expected for this operation
+  res.end();
+}
+
+exports.partial_handler = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  **/
+  // no response value expected for this operation
+  res.end();
+}
+
